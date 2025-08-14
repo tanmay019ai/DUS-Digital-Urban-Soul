e3ae40c1b4e7ea712b0d1a0ecd228744b39a1e8f
tanmay019ai <tanmaysr019@gmail.com>
2025-08-14 16:53:07 +0530

    code

---
+++ urban-soul/src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "../index.css";
+import lightningVideo from "../assets/120382-720505197_large.mp4";
+
+export default function Hero() {
+  return (
+    <div className="hero-container">
+      {/* Lightning background video */}
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="lightning-video"
+      >
+        <source src={lightningVideo} type="video/mp4" />
+      </video>
+
+      {/* Typing animation text */}
+      <div className="typing-text">
+        WELCOME TO URBAN SOUL, THE CITY THAT HOLDS YOUR KARMA
+      </div>
+    </div>
+  );
+}
